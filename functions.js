e0c40773f5921e9cd42df70f5ae14526daa057f7
Elijah Kin <3limkin@gmail.com>
2021-03-27 12:59:14 -0400

    Added functionality for iterating through the rooms

--- functions.js
+++ functions.js
@@ -1,10 +1,41 @@
+// Sets up memory palace after text is inputted
 function go() {
+    roomIter = 0;
+    roomCount = document.getElementById("memtext").value.length - 1;
+    updateRoom();
     swapVisibility();
 }
 
 // Hides input text box and button, unhides output text, canvas, and controls
 function swapVisibility() {
-    input = document.getElementById("input");
-    input.style.display = "none";
-    output.style.display = "inline";
+    document.getElementById("input").style.display = "none";
+    document.getElementById("output").style.display = "inline";
+}
+
+// Updates transition text and image with current value of roomIter
+function updateRoom() {
+    document.getElementById("transition").innerHTML = roomIter;
+    document.getElementById("image").src = "images/orange.png";
+}
+
+// Increments roomIter by one, calls updateRoom
+function next() {
+    if (roomIter < roomCount) {
+        roomIter += 1;
+        updateRoom();
+    }
+}
+
+// Decrements roomIter by one, calls updateRoom
+function prev() {
+    if (roomIter > 0) {
+        roomIter -= 1;
+        updateRoom();
+    }
+}
+
+// Sets roomIter to zero, calls updateRoom
+function restart() {
+    roomIter = 0;
+    updateRoom();
 }
